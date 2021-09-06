c88a3e63becd5e69cb43aac5298069982306fd0f
Acubenso <moskalvasyljsdev@gmail.com>
2021-09-06 05:22:14 +0300

    First commit

---
+++ main.js
@@ -0,0 +1,5 @@
+function toggleClassFunc (){
+        //document.querySelector('.burger').click(function(event) {
+                $('.content-title__box').toggleClass('open');
+            //});
+};
